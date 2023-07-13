f79130a0373ef328f1d9bb9066a7ddac7c5683d6
Dmitriy <kagno.dmitriy@gmail.com>
2023-07-12 22:33:16 -0400

    chore: add ts-reset to project

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
+import '@total-typescript/ts-reset';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+declare global {
+	declare namespace App {
+		// interface Locals {}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
+	}
 }
