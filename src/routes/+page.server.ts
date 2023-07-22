f2a17e05093660feeb4cc4f447d49d888061906c
Dmitriy <kagno.dmitriy@gmail.com>
2023-07-22 19:21:21 -0400

    chore: update initial load and logout logic

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,30 +1,27 @@
-import { fail, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 import { auth } from '$lib/server/auth';
 
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	async function getUser() {
+	const getUser = async () => {
 		const { user } = await locals.auth.validateUser();
 		return user;
-	}
+	};
 
 	return {
-		user: getUser()
+		user: getUser(),
 	};
 };
 
 export const actions: Actions = {
 	logout: async ({ locals }) => {
-		const { session, user } = await locals.auth.validateUser();
-
-		if (!user) {
-			return fail(401);
+		if (locals.auth.storedSessionId) {
+			await auth.invalidateSession(locals.auth.storedSessionId);
+			locals.auth.setSession(null);
 		}
 
-		await auth.invalidateSession(session.sessionId); // invalidate session
-		locals.auth.setSession(null); // remove cookie
-		throw redirect(302, '/'); // redirect to login page
+		throw redirect(302, '/');
 	}
 };
