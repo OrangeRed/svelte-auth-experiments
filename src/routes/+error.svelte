e7fe870811517fa2b28c902ef33e111af7def5b9
Dmitriy <kagno.dmitriy@gmail.com>
2023-07-24 12:17:15 -0400

    chore: clean up home page and add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div>
+	{#if $page.status === 404}
+		<h1>Page Not Found.</h1>
+		<h3 class="mt-6"><a href="/">Go Home</a></h3>
+	{:else}
+		<h1 class="h1">Unexpected Error</h1>
+		<h3 class="mt-6">We're investigating the issue.</h3>
+	{/if}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,43 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+
+	import type { PageData } from './$types';
+
+	import { page } from '$app/stores';
+
+	export let data: PageData;
 </script>
 
-<!-- App Shell -->
 <AppShell>
-	<slot />
+	<!-- Header -->
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Svelte Auth Experiment</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				{#if !data.user}
+					<a class="btn variant-filled" href="/sign-in">Login</a>
+				{:else}
+					<form method="POST" action="?/logout">
+						<button type="submit" class="btn variant-filled">Logout</button>
+					</form>
+				{/if}
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<!-- Main Content -->
+	<main class="container mx-auto flex h-full">
+		<slot />
+	</main>
+
+	<!-- Footer -->
+	<svelte:fragment slot="footer">
+		<div class="m-8 flex flex-row justify-center">
+			<a href="/">Home</a>
+		</div>
+	</svelte:fragment>
 </AppShell>
